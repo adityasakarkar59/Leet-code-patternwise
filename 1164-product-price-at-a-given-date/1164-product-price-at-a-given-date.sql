4f96490a19df787ef2cc891e3bcd2fa3e4958122
Aditya Sakarkar <adityasakarkar781@gmail.com>
2026-06-28 00:15:34 +0530

    Time: 711 ms (16.97%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,9 @@
+select 
+    p.product_id,
+    ifnull( (select p2.new_price
+                from Products p2
+                 where p2.product_id=p.product_id
+                 and p2.change_date <= '2019-08-16'
+                 order by p2.change_date desc limit 1),10) as price
+from 
+  (select distinct product_id from Products)p;
