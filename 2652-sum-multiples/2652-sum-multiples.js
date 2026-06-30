1d1cf86d7f88776af1bb486792962f7aaff7aa77
Aditya Sakarkar <adityasakarkar781@gmail.com>
2026-06-30 23:47:50 +0530

    Time: 0 ms (100%), Space: 57.2 MB (13.04%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+    var sum=0;
+    for(var i=1;i<=n;i++){
+        if(i%3===0 || i%5===0 || i%7===0){
+            sum+=i;
+        }
+    }
+    return sum;
+};
